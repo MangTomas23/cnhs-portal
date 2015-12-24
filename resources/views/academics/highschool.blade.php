a24f4195a1590a4cf3957563b5af61d411afb793
MangTomas23 <matos.adrianpaul@gmail.com>
2015-12-24 18:11:37 +0800

    Add contents to highschool/senior high

--- resources/views/academics/highschool.blade.php
+++ resources/views/academics/highschool.blade.php
@@ -2,6 +2,143 @@
 
 @section('content')
 
-<h2>Highschool</h2>
+<img src="/img/high.png" style="width: 120%; margin-left: -15px;">
+
+<h1>HIGHSCHOOL</h1>
+<hr>
+
+<h2> Rules And Regulations </h2>
+
+<h2>General Behavior in the Classroom</h2>
+
+<ol type="a">
+	<li>Students should endeavor to be inside their classrooms on time and 
+	actively participate in class discussion.</li>
+	<li>Student should always practice courtesy and politeness to school 
+	personnel, visitors and classmates.</li>
+	<li>Students should be attentive in class and refrain from doing unnecessary 
+	acts that may disturb the orderly flow of the lesson.</li>
+	<li>At the end of the period, students should remain seated until their 
+	teacher dismiss them.</li>
+	<li>Students should always observe proper decorum inside the classroom, even 
+	if the teacher assigned for the period has not arrived yet.</li>
+	<li>Students should comply with the seating arrangement that maybe 
+	prescribed, and maintain the orderliness and cleanliness of the classroom.</li>
+</ol>
+
+<h2>General behavior inside the campus</h2>
+
+<ol type="a">
+	<li>All students shall wear their uniforms, hang their identification cards around their neck while inside the campus. Those who refuse or cannot show their identification card may, in appropriate cases be sent out of the premises.</li>
+	<li>Students are expected to maintain/ help maintain peace and order, and make positive efforts to keep the campus clean and attractive. Proper use of garbage cans should observed.</li>
+	<li>While attending any school program/ activity and other social functions, students should refrain from making unnecessary noise or causing other disturbances. They shall observe the usual courtesy of attending the program from start to finish, and just leave after the last number had been delivered. They shall not leave or enter the hall while a number is being presented.</li>
+	<li>Courtesy and politeness should always be observed with everyone.</li>
+	<li>Approaching a teacher to certain grade, or to request for a change of a grade already given is prohibited. A student may however inquire from the faculty member concern how he got his grade. Compliant or request of any nurture regarding grades must be cursed through the principal or the registrar.</li>
+	<li>A grade once submitted to the principal or the registrar, cannot be altered or modified except with the written approval of the principal or the registrar upon certification in writing by the teachers concerned to the effect that he has committed an error in giving the grades. The certification should also state how the error was committed.</li>
+</ol>
+
+<h2>General behavior outside the campus</h2> <br><br>
+
+<ol type="a">
+	<li>School reserves  the right to take proper disciplinary measures against any students for acts committed outside the campus which tend to impair its good name or expose it to public contempt and ridicule even when the student does not represent or claim to represent it or any of its organizations.</li>
+	<li>School dos not assume ay responsibly for untoward incidents beyond its control which may happen to students during out of campus activities such as fieldtrip, tours, picnics, excursions, etc. students who voluntarily join out of campus activities organized by the school will be required to present o written permission from their parents or guardian before they are allowed to join or participate.</li>
+	<li>School does not allow out of campus activity by any group of stun=dents or student organization except by special written permission from the proper school/DepEd authorities subject to the conditions it may impose. All person in charge of such activities are enjoined to strictly comply with such conditions.</li>
+</ol>
+
+<h2>Miscellaneous provisions</h2>
+	
+	<ol type="a">
+		<li>Examination rules and regulations </li>
+		<ul>
+			<li>Cheating of any kind in any test or quiz merits a zero score or a 
+			failing grade </li>
+			<li>Before the start of examination , a student must place his belongings 
+			as books, or notes under his seat or at some designated place as maybe 
+			ordered by  the teacher/proctor</li>
+			<li>A student is not allowed to leave the examination room after the 
+			examination has begun without submitting his answer sheet which he should 
+			not get back afterwards for purpose of  changing or supplementing his 
+			answer.</li>
+			<li>A student must familiarize himself with the grading system and rules 
+			on special examinations</li>
+		</ul>		
+	</ol>
+
+
+<h3>Calabanga National High School History</h3>
+
+<p>Established several decades ago, Calabanga National High School is now occupying it’s new, spacious, and elevated two-hectare school campus located at Sta. Cruz, Calabanga, Camarines sur, about 1-5 kilometers away from the Poblacion or 20mins drive from Naga city via the national high-way.<p>
+
+
+
+
+
+<p>Municipal resolution No. 60, series of 1996 sponsored by the councilor David M. Hernandez during the administration of Mayor Dominador V. Medroso, established the Calabanga High school in July 1966. It was housed in Calabanga Central Division pilot school, and was called Camarines Sur high school annex. The pioneer Administrator was Mr. Pedro O. Bornas, principal of mother high school - Camarines Sur high school, Naga city during the incumbency of Jaime T. Malanyaon, the provincial Superintendent of schools for the division of Camarines Sur. However, Mrs. Rosario P. Galicia and Mrs. Paula M. Nagrampa, District Supervisors and central principals respectively, helped in the administration and supervision of the school. Its operation began in school year 1966-1967 with classes up to second year having eight teachers and a total of 185 students.<p>
+
+
+
+
+
+<p>In 1970, municipal resolution no. 63, series of the same year has been passed requesting the higher authorities of the Department of Education to make Calabanga High School independent and distinct from the mother high school. The request was finally approved in 1971 and took effect beginning school year 1971-1972 and was named Calabanga high school.<p>
+
+
+
+
+
+
+<p>Mr. Diosdado Silvestre I, the Guidance Counselor of Camarines Sur High School became the full-fledged principal. The school was transferred to another site owned by the Municipal government of Calabanga, about 100 meters from the town plaza which is now the municipal market. During that time, the school had twelve teachers, a guidance counselor and a school janitor. The torchbearer-official organ with Mr. Mariano Ariola, the guidance counselor acted as adviser was also published.<p>
+
+
+
+
+
+
+<p>As a barrio high school, then a municipal high school by virtue of department circular No. 65, s. 1975, it suffered problem of weak financial structure, probably the weakest among the three (3) levels of education. To solve this problem, several concerted attempts starting from the administration of Mrs. Nieves P. Badiola to Mr. Pedro E. Bermudo, Jr. To convert the school to a national high school were made but did not succeed. Later on, the municipal council passed a resolution proposing the fusion of Calabanga High School with Calabanga school arts and trades with the end view of addressing teacher’s problems on salaries and poor school facilities. Majority of the teachers, parents and students did not approved the idea, thus the proposal was dropped. The reaction was indicative of a rare choice, willingness to suffer rather than lose identity of Calabanga High School. <p>
+
+
+
+
+
+
+
+<p>After so many long years of suffering from financial crisis, finally in 1998, under the Aquino administration, the government had a turn-about on its program in education. It became the first priority, and free public secondary education was implemented by republic act 6655 effective school year 1988-1989. The financial needs of the school were taken care of by the nation government. Salaries were upgraded, standardized and released on time. New facilities were constructed and more textbook were printed and distributed. So that the school would be assured of continued progress, new school buildings were constructed but unfortunately, Calabanga High School was not among the recipients. To construct a new one, it became a matter of policy that standard school site must be owned. Toward this end, the school administration under the leadership of Dr. Teresita S. Beriña, the OIC - principal, with the support of the teachers, parents and students acquired a new elevated two-hectare school campus situated at Sta. Cruz-Ratay, Calabanga, Camarines Sur, now its permanent campus, it was in 1955 that the night and summer classes were dissolved for the reasons that secondary education was already free.<p>
+
+
+
+
+
+
+<p>However, to further democratize access to secondary education, night classes were again opened during the school year 2002-2003.<p>
+
+<p>In 1991, Dr. Teresita S. Beriña was promoted to principal II at Nabua National High School. She was succeeded by Mr. William A. Villare, a head Teacher from Sto. Tomas High School, Camaligan, Camarines Sur, who later became the school principal. Like his predecessors, he worked hard to improve the quality of instruction side by side with the improvement of physical facilities such as the construction of new school buildings, the famous of which was the two-storey SEDP-SBP school building in 1993, the acquisition of brand new equipment and apparatuses for science, technology and livelihood education, and other areas of learning.  <p>
+
+
+<p>Through the years Calabanga National high school has undergone metamorphosis from a mere annex school of Camarines Sur National High School in Naga city to a progressive educational institution that in turn has given birth to six (6) annex schools under the Administration of Mr. William a. Villare. In 1989 the first annex school named union high school in Sto Domingo, Calabanga  Camarines sur started its operation with Mrs. Emily O. Amon as its pioneer teacher-in-charge. Medroso-Mendoza high school in Binanuanan Pequeño headed by Mrs. Estrella F. Belleca and Jose de villa Memorial High School headed by Mrs. Maria P. Gueriba were simultaneously established in 1994. And in 1997, three (3) more annex schools were established- Sabang High School in Sabang Calabanga headed by Miss Natividad Q. Carillo (now Mrs. De Leon) Quipayo High School in San Antonio, Quipayo headed by Mrs. Silvia D. Obias, and West Coast High School in Dominorog Calabanga first headed by Mr. Redendor S. Malanyaon, then by Mr. Samuel A. Salalima and by Mr. Lope C. Espiritu. In august , 2000 four (4) of the six (6) annex schools started working out for legislation as national high school and were able to operate independently. West Coast high school and Sabang High school are also on process of separation.<p>
+
+
+
+
+<p>When Mr. William A. Villare was assigned at pili National high school, pili, Camarines sur on October 1, 2004, Mrs. Emily B. Esmabe succeeded him. On July 5, 2005, Dr. Lilian Paredes - Rubi became the newest principal of the school. Modern equipment were produced, IT - Instruction were fully implemented, more and more buildings were added, to accommodate the expanding academic population and the campus was given a total makeover in terms of landscaping. Ad through the days , positive change in all areas of learning were observable , as this school is committed to be catalyst of progress through secondary education at every doorstep, and will continue to uphold the maxim : " Let knowledge grow from to more and thus must be human life enriched". Truly, academic excellence is not an elusive dream to realize, when everybody's effort is put together for realistic, effective and consistent development through the participatory management.<p>
+
+
+
+
+
+<p>The evolution of the school's name from Camarines Sur High School annex in 1966 to Calabanga community high school in 1969, to Calabanga community High School in 1969, to Calabanga High School in 1971 and finally Calabanga National high school in 2001 became a trademark of excellence, as every change depicted remarkable struggle and victory to cast off ignorance and literacy. Now, tested by time Calabanga national high school stands firm, stable and united in its advocacy. <b>Long live!</b><p>
+
+
+
+<br><br><br><br>
+
+<h2>PHILOSOPHY AND OBJECTIVE OF THE SCHOOL</h2>
+
+<p>The Camarines Sur National High School is committed to the pursuit of excellence in secondary education.<p>
+<p>The school is open to all elementary school graduates regardless of their academic achievements and socio-economic status because it believes that everyone deserves chance to pursue/finish secondary education and to prove their worth as good students and useful citizens.<p>
+
+<p>The school offers the ALS (alternative learning school) for the out of school youth and drop-outs, incumbent students who are 15 years old and above who prefer to take this course, and to the working students to provide them equal opportunity for education.<p>
+
+<p>In addition, CNHS offers evening classes for the working students. A complete 5 year level curriculum bas been existing to assist the said students and parents as well.<p>
+
+<p>It also believes that the noble task for educating the youth is not only the responsibility of the school but also of the stakeholders such as the parents, local government and non-government organizations.<p>
 
 @endsection
--- resources/views/academics/senior-highschool.blade.php
+++ resources/views/academics/senior-highschool.blade.php
@@ -2,6 +2,126 @@
 
 @section('content')
 
-<h2>Senior Highschool</h2>
+<div class="jumbotron">
+	
+	<h2> Calabanga National Highscool is now accepting senior high ! </h2>
+
+  <h3> Academic track </h3>
+
+	<div class="panel panel-primary">
+	  <div class="panel-heading"> Accountancy, Business and Management (ABM) 
+	  Strand</div>
+	  <div class="panel-body">
+    	<ol>
+				<li><a href="http://www.deped.gov.ph/sites/default/files/ABM_Applied%20Economics%20CG_4.pdf">Applied Economics</a></li>
+				<li><a href="http://www.deped.gov.ph/sites/default/files/ABM_Business%20Ethics%20and%20Social%20Responsibility%20CG_2.pdf"> Business Ethics and Social Responsibility</a></li>
+				<li><a href="http://www.deped.gov.ph/sites/default/files/ABM_Fundamentals%20of%20Accountancy%2C%20Business%2C%20and%20Management%201_2.pdf"> Fundamentals of Accountancy, Business and Management 1</a></li>
+				<li><a href="http://www.deped.gov.ph/sites/default/files/ABM_Fundamentals%20of%20Accountancy%2C%20Business%2C%20and%20Management%202_2.pdf"> Fundamentals of Accountancy, Business and Management 2</a></li>
+				<li><a href="http://www.deped.gov.ph/sites/default/files/ABM_Business%20Math%20CG_2.pdfl"> Business Math</a> </li>
+				<li><a href="http://www.deped.gov.ph/sites/default/files/ABM_Business%20Finance%20CG_2.pdf"> Business Finance</a></li>
+				<li><a href="http://www.deped.gov.ph/sites/default/files/ABM_Organization%20and%20Management%20CG_4.pdf"> Organization and Management</a></li>
+				<li><a href="http://www.deped.gov.ph/sites/default/files/ABM_Principles%20of%20Marketing%20CG_2.pdf"> Principles of Marketing </a></li>
+				<li>Work Immersion/Research/Career Advocacy/Culminating Activity i.e. <a href="http://www.deped.gov.ph/sites/default/files/ABM_Culminating%20Activity_Business%20Enterprise%20Simulation%20CG_2.pdf">Business Enterprise Simulation</a></li>
+    	</ol> 
+		</div>
+	</div>  
+
+	<div class="panel panel-primary">
+  	<div class="panel-heading">Humanities and Social Sciences (HUMSS) Strand</div>
+    <div class="panel-body">
+    	<ol>
+    		<li><a href="http://www.deped.gov.ph/sites/default/files/HUMSS_Creative%20Writing%20CG_1.pdf"> Creative Writing </a>/<a href="http://www.deped.gov.ph/sites/default/files/HUMSS_Malikhaing%20Pagsulat%20CG.pdf"> Malikhaing Pagsulat </a></li>
+    		<li><a href="http://www.deped.gov.ph/sites/default/files/page/2015/HUMSS_Introduction%20to%20World%20Religions%20%26%20Belief%20Systems%20CG.pdf">Introduction to World Religions and Belief Systems</a></li>
+    		<li><a href="http://www.deped.gov.ph/sites/default/files/page/2015/HUMSS_Creative%20Nonfiction%20CG.pdf"> Creative Nonfiction</a></li>
+    		<li><a href="http://www.deped.gov.ph/sites/default/files/HUMSS_Trends%2C%20Networks%2C%20and%20Critical%20Thinking%20in%20the%2021st%20Century%20Culture%20CG_1.pdf"> Trends, Networks, and Critical Thinking in the 21st Century Culture</a></li>
+    		<li><a href="http://www.deped.gov.ph/sites/default/files/HUMSS_Philippine%20Politics%20and%20Governance%20CG_1.pdf"> Philippine Politics and Governance</a></li>
+    		<li><a href="http://www.deped.gov.ph/sites/default/files/HUMSS_Community%20Engagement%2C%20Solidarity%20and%20Leadership%20CG_1.pdf"> Community Engagement, Solidarity, and Citizenship</a></li>
+    		<li><a href="http://www.deped.gov.ph/sites/default/files/HUMSS_Disciplines%20and%20Ideas%20in%20the%20Social%20Sciences%20CG__1.pdf"> Disciplines and Ideas in the Social Sciences</a></li>
+    		<li><a href="http://www.deped.gov.ph/sites/default/files/HUMSS_Disciplines%20and%20Ideas%20in%20the%20Applied%20Social%20Sciences%20CG_1.pdf"> Disciplines and Ideas in the Applied Social Sciences</a></li>
+    		<li><a href="http://www.deped.gov.ph/sites/default/files/HUMSS_Culminating%20Activity%20CG_1.pdf">   Work Immersion/Research/Career Advocacy/Culminating Activity</a></li>
+    	</ol>
+    </div>
+  </div>
+	<div class="panel panel-primary">
+    <div class="panel-heading">Science, Technology, Engineering and Mathematics (STEM) Strand</div>
+    <div class="panel-body">
+    	<ol>
+	    	<li><a href="http://www.deped.gov.ph/sites/default/files/STEM_Pre-Calculus%20CG_1.pdf"> Pre-Calculus</a></li>
+	    	<li><a href="http://www.deped.gov.ph/sites/default/files/STEM_Basic%20Calculus%20CG_1.pdf"> Basic Calculus</a></li>
+	    	<li><a href="http://www.deped.gov.ph/sites/default/files/STEM_General%20Biology%201%20CG_1.pdf">General Biology 1</a></li>
+	    	<li><a href="http://www.deped.gov.ph/sites/default/files/STEM%20General%20Biology%202%20CG_1.pdf"> General Biology 2</a></li>
+	    	<li><a href="http://www.deped.gov.ph/sites/default/files/STEM_Physics%201%20CG_1.pdf"> General Physics 1</a></li>
+	    	<li><a href="http://www.deped.gov.ph/sites/default/files/STEM_Physics%202%20CG_1.pdf"> General Physics 2</a></li>
+	    	<li><a href="http://www.deped.gov.ph/sites/default/files/STEM_General%20Chemistry%201%20and%202%20CG_1.pdf"> General Chemistry 1 and 2</a></li>
+				<li><a href="http://www.deped.gov.ph/sites/default/files/STEM_Research%20or%20Capstone%20Project%20CG_1.pdf"> Research/Career Advocacy/Culminating Activity</a></li></li>
+    	</ol>
+    </div>
+  </div> 
+
+  <div class="panel panel-primary">
+    <div class="panel-heading">General Academic Strand</div>
+    <div class="panel-body">
+    	<ol>
+    		<li>Humanities 1*</li>
+    		<li>Humanities 2*</li>
+    		<li>Social Science 1**</li>
+    		<li><a href="http://www.deped.gov.ph/sites/default/files/ABM_Applied%20Economics%20CG_4.pdf">Applied Economics</a></li>
+    		<li><a href="http://www.deped.gov.ph/sites/default/files/ABM_Organization%20and%20Management%20CG_2.pdf">Organization and Management</a></li>
+    		<li><a href="http://www.deped.gov.ph/sites/default/files/SHS%20Core_Disaster%20Readiness%20and%20Risk%20Reduction%20CG_2.pdf">Disaster Readiness and Risk Reduction</a></li>
+    		<li>Elective 1 (from any Track/Strand)***</li>
+    		<li>Elective 2 (from any Track/Strand)***</li>
+    	</ol>
+    </div>
+  </div>
+    
+  <div class="row">
+    <div class="col-sm-4">
+    	<i class="fa fa-balance-scale" style="font-size: 100px;"></i>
+      <p>Accountancy, Business and Management(ABM)</p>
+    </div>
+    <div class="col-sm-4">  
+      <i class="fa fa-bar-chart" style="font-size: 100px;"></i>
+      <p>General Academic (GA)</p>
+    </div>
+    <div class="col-sm-4"> 
+      <i class="fa fa-flask" style="font-size: 100px;"></i>
+      <p>Science and Technology , Engineering and Mathematics,(stem)</p>
+    </div>
+  </div>
+
+
+    
+    
+
+  <h3>TECH-VOC</h3>
+  <hr>
+  <div class="row">
+    <div class="col-sm-3">
+      <span class="fa fa-scissors" style="font-size: 100px;"></span>
+      <p>Dressmaking</p>
+    </div>
+    <div class="col-sm-3">  
+      <span class="fa fa-cutlery" style="font-size: 100px;"></span>
+      <p>Food Processing</p>
+    </div>
+    <div class="col-sm-3"> 
+      <span class="fa fa-bolt" style="font-size: 100px;"></span>
+      <p>Electrical Installation</p>
+    </div>
+     <div class="col-sm-3"> 
+      <span class="fa fa-desktop" style="font-size: 100px;"></span>
+      <p> Computer System Serving and Maintenance </p>
+    </div>
+  </div>
+</center>
+
+
+	<h3 style="text-align:center">For More Information about senior high go here 
+		<a href="http://www.deped.gov.ph/k-to-12/curriculum-guides/Academic-Track"> 
+			<span class="fa fa-fire"></span> 
+		</a> 
+	</h3>
+
+</div>
 
 @endsection
