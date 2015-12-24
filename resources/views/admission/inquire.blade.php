eb381c86f819d53e9d3a44e6795756e3cbc9965e
MangTomas23 <matos.adrianpaul@gmail.com>
2015-12-24 15:44:44 +0800

    Add contents to admission.inquire

--- resources/views/admission/inquire.blade.php
+++ resources/views/admission/inquire.blade.php
@@ -1,7 +1,98 @@
 @extends('master')
 
 @section('content')
-	
-	<h2>Inquire</h2>
 
+<div style="padding: 20px;">	
+	<h2>Inquire <span class="fa fa-search" style= "font-size: 30px;"></span></h2>
+
+	<h3>ACADEMIC POLICIES</h3> 
+
+	<p>It is the policy of the school to provide for a free Public Secondary 
+	Education (D.O. No. 44, s. 1988). Conformably, the following shall be observed.
+
+	<ul>
+			<li> S/Y 2015-2016</li> 
+			<li> Grade 7 – K to 12 Curriculum</li>
+			<li> Second year – secondary education curriculum (SEC)</li>
+			<li> Third year & Forth year – basic education curriculum </li>
+			<li> STOC (Science and technology oriented Class) – Special Science 
+			Curriculum (SSC)</li>
+	</ul>
+
+	<h3> ADMISSION POLICY </h3>
+		<p> SEC – Secondary education curriculum /BEC – basic education curriculum</p>
+		<p> No Filipino student shall be refused admission by reason of gender, greed, 
+		socio-economic status, racial and ethnic origin, political and other 
+		affiliation, in the public school system. However, due to limited space in the 
+		particular school some students may be referred to some other public schools 
+		preferable within the community or they may be referred to private schools 
+		under the educational service contracting scheme. Admission to central 
+		schools and schools identified as leaders schools in science, math, English, 
+		shall be based on established criteria.</p>
+
+		<p> STOC - Science and Technology Oriented Class</p>
+		<p> A student must:</p>
+		<ul>
+			<li>Pass the SSC qualifying exam.</li>
+			<ul>
+				<li>written examination, essay test, and interview</li>
+				<li>Final interview and screening</li>
+			</ul>			
+			<li>Have inclination in science and mathematics</li>
+		</ul>
+
+	<h3> ENROLLMENT SCHEDULE </h3>
+		<p> To give enough time to students, enrollment starts 2 weeks before the 
+		opening of classes simultaneously in all levels; in addition, grade 7 shall 
+		conduct early enlistment/ registration every last week of January or whenever 
+		scheduled by the department of education.</p>
+		<p> Requirements for enrollment: </p>
+			<p> GRADE 7 </p>
+			<ol>
+				<li>Report card </li>
+				<li>Good moral certificate</li>
+				<li>one (1) pc. Long size brown envelop</li>
+				<li>Birth certificate (clear copy)</li>
+				<li>DepEd form 137-E (Elementary Permanent Record)</li>
+				<li>3 pcs. 2X2 ID Picture (colored Blue Background, in school 
+				polo/blouse uniform)</li>
+			</ol>
+			<p> For old Students (Enlistment)</p>
+			<ol>
+				<li>Report card</li>
+				<li>Grades for summer classes, if with failing grades</li>
+				<li>Good moral certificate (if so required)</li>
+				<li>1pc. 2X2 ID Picture</li>
+			</ol>
+			
+			<p> Transferee</p>
+				<ol>>
+					<li>Report card (DepEd form 138A)</li>
+					<li>Good moral certificate</li>
+					<li> One (1) pc long size brown envelop</li>
+					<li>Birth certificate (clear copy)</li>
+					<li>DepEd form 137-A (Secondary permanent record)</li>
+					<li>3 pcs 2X2 ID Pictures (colored blue background, in school 
+					polo/blouse uniform)</li>
+					<li>Barangay clearance</li>
+				</ol>
+				<small>Note: male student should be in proper haircut and refrain from 
+				wearing earrings.</small>
+	<br>
+	<br>
+	<br>
+			<h3> GRADING SYSTEM </h3>
+	<br>
+			<p> The school year is divided into 4 grading periods. Evaluation of 
+			student’s performance at the end of each quarterly period is usually held 
+			on the following months in the form of periodical examinations:</p>
+			<p> 1st Quarter – August</p>
+			<p> 2nd Quarter – October</p>
+			<p> 3rd Quarter – January</p>
+			<p> 4th Quarter – March</p>
+				<br>
+			<small>Note: updates in grading system issued by DepEd will be 
+			disseminated the homeroom PTA meeting.</small>
+
+</div>
 @endsection
