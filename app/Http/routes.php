4e20c9807ea84f27638f97837f6b5aa64b01b5a6
MangTomas23 <matos.adrianpaul@gmail.com>
2015-12-24 14:51:21 +0800

    Add about to routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('about', function() {
+	return view('index');
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
