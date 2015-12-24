9c0cc7498009854ee36454af24da8811210bd695
MangTomas23 <matos.adrianpaul@gmail.com>
2015-12-24 16:32:59 +0800

    Add contents to library

--- resources/views/academics/library.blade.php
+++ resources/views/academics/library.blade.php
@@ -2,6 +2,40 @@
 
 @section('content')
 
-<h2>Library</h2>
+<h1> Library <span class="fa fa-book"></span> </h1>
+
+<div class="jumbotron">
+	<h4>Library Rules and Regulations</h4>
+		<ol>
+			<li>Students are required to have a library card and use it for 
+			borrowing books and other references materials</li>
+			<li>The library card is not transferable</li>
+			<li>Periodical, general references and reserved book must be read 
+			inside the library.</li>
+			<li>Circulation books may be taken for overnight at 4:00 p.m. and 
+			returned before 10:30 am of the following day. Over noon use of books may 
+			be taken at 10:00 am and should be return not later than 2:00pm of the 
+			same day. </li>
+			<li>For overdue use of books the librarian should require the students 
+			to submit written explanation of attested by their parent/guardian.</li>
+			<li>Lost books must be reported at once and be replaced or paid for by 
+			the student in the school cashier.</li>
+			<li>Taking out books without permission is stealing and punishable by 
+			law.</li>
+			<li>Students, who deliberately mark, vandalize, mutilate or deface any 
+			book or library material will be subjected to disciplinary action.</li>
+			<li>Students are expected to observe the following: </li>
+
+			<ol type="a" style="background: #FFF; padding: 10px auto; border-radius: 5px; margin-top: 12px;">
+				<li>Take good care of the borrowed materials.</li>
+				<li>Keep quite while passing the library premises and when inside the library.</li>
+				<li>Return things in their proper places after using.</li>
+				<li>Refrain from eating inside the library and chitchatting with each other.</li>
+				<li>Follow the library rules and regulations at all times</li>
+			</ol>
+
+		</ol>
+</div>
+
 
 @endsection
