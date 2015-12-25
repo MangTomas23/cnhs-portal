f311d25306ad120f583d87a990e80a59ee0d9a07
MangTomas23 <matos.adrianpaul@gmail.com>
2015-12-25 13:33:21 +0800

    Add more columns to users table

---
+++ database/migrations/2015_12_25_052604_add_columns_for_users.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsForUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('firstname');
+            $table->string('middlename')->nullable();
+            $table->string('lastname');
+            $table->integer('year_level');
+            $table->string('gender');
+            $table->date('birthdate');
+            $table->string('address')->nullable();
+            $table->string('contact')->nullable();
+            $table->string('position')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('firstname');
+            $table->dropColumn('middlename');
+            $table->dropColumn('lastname');
+            $table->dropColumn('year_level');
+            $table->dropColumn('gender');
+            $table->dropColumn('birthdate');
+            $table->dropColumn('address');
+            $table->dropColumn('contact');
+            $table->dropColumn('position');
+        });
+    }
+}
