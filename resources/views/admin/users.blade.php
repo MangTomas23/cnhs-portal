df02f18633e074585cb640825b5ca6dce92ac908
MangTomas23 <matos.adrianpaul@gmail.com>
2015-12-25 23:05:47 +0800

    Add user nav

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -2,11 +2,18 @@
 
 @section('content')
 
+<ul class="nav nav-tabs">
+  <li role="presentation" class="active"><a href="#">Students</a></li>
+  <li role="presentation"><a href="#">Teachers</a></li>
+  <li role="presentation"><a href="#">Program Head</a></li>
+</ul>
+
 <table class="table table-default">
 	<thead>
 		<th>Username</th>
 		<th>Name</th>
 		<th>Address</th>
+		<th>Subjects</th>
 	</thead>
 	<tbody>
 		@foreach($users as $user)
@@ -14,6 +21,9 @@
 				<td>{{ $user->username }}</td>
 				<td>{{ join(' ', [$user->firstname, $user->middlename, $user->lastname]) }}</td>
 				<td>{{ $user->address }}</td>
+				<td>
+					MAPEH, ENGL02
+				</td>
 			</tr>
 		@endforeach
 	</tbody>
