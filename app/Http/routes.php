c9fe5fe1eda3d79f190f15fac582218426a35aa4
MangTomas23 <matos.adrianpaul@gmail.com>
2015-12-27 17:17:20 +0800

    Fix artisan call

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -168,7 +168,7 @@ Route::group(['middleware' => 'web'], function () {
     });
 
     Route::get('developer/run-seed', function () {
-        Artisan::call('db:seed -n');
+        Artisan::call('db:seed',['--no-interaction' => 'n']);
 
         return Artisan::output();
     });
