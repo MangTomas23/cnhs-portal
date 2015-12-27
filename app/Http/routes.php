03bf86de95e75e30e9f72e69b061887e33b08bdf
MangTomas23 <matos.adrianpaul@gmail.com>
2015-12-27 17:26:57 +0800

    Fix command

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -168,7 +168,7 @@ Route::group(['middleware' => 'web'], function () {
     });
 
     Route::get('developer/run-seed', function () {
-        Artisan::call('db:seed',['--force' => '']);
+        Artisan::call('db:seed',['--force' => 'y']);
 
         return Artisan::output();
     });
