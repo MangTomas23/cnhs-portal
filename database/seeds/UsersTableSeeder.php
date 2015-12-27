4c392b4f106ef50f50650b68a60a4679cf00b716
MangTomas23 <matos.adrianpaul@gmail.com>
2015-12-27 20:38:51 +0800

    Add timestamps

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,7 +18,9 @@ class UsersTableSeeder extends Seeder
             'firstname' => 'admin',
             'lastname' => 'admin',
             'gender' => 'm',
-        	'birthdate' => '1994-11-13'
+            'birthdate' => '1994-11-13',
+            'created_at' => '2015-12-27 10:40:19',
+        	'updated_at' => '2015-12-27 10:40:19',
         ]);
     }
 }
