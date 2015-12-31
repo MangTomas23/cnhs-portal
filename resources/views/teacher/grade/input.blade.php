4e53cf8b268e754a8a10b65144c8c806a7d95c64
MangTomas23 <matos.adrianpaul@gmail.com>
2015-12-31 09:58:14 +0800

    Add contents

--- resources/views/teacher/grade/input.blade.php
+++ resources/views/teacher/grade/input.blade.php
@@ -17,7 +17,49 @@
 					<option>No sections</option>
 				@endif
 			</select>
-		</div>		
+		</div>
+		<div class="form-group">
+			<label for="subject">Subject</label>
+			<select name="subject" class="form-control">
+				@if(count($subjects))
+					@foreach($subjects as $subject)
+						<option value="{{$subject->subject->id}}">{{ $subject->subject->subject_code }}</option>
+					@endforeach
+				@else
+					<option>No subjects</option>
+				@endif
+			</select>
+		</div>
+
+		<div class="form-group">
+			<label for="school_year">School Year</label>
+			<select name="school_year" class="form-control">
+				<option>2015-2016</option>
+				<option>2016-2017</option>
+				<option>2017-2018</option>
+			</select>
+		</div>
+
+		<strong>Students</strong>
+
+		<table class="table table-default">
+			<thead>
+				<th>#</th>
+				<th>Student Name</th>
+				<th>1st Quarter</th>
+				<th>2nd Quarter</th>
+				<th>3rd Quarter</th>
+				<th>4th Quarter</th>
+				<th>Average</th>
+			</thead>
+			<tbody>
+				
+			</tbody>
+		</table>
+
+		<div class="alert alert-danger">
+			Development. In-progress
+		</div>
 	</form>
 </div>
 
