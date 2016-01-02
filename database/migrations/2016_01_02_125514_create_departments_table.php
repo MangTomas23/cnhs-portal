3693f39401ec6ad81b5c7dbce6b06e9c5bb54a08
MangTomas23 <matos.adrianpaul@gmail.com>
2016-01-02 20:59:03 +0800

    Add Department model

---
+++ database/migrations/2016_01_02_125514_create_departments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->integer('program_head')->unsigned()->nullable();
+            $table->foreign('program_head')->references('id')->on('users')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('departments');
+    }
+}
