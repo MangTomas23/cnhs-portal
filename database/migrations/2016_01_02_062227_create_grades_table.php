d98399a0cdd892b7e2e3398ae2ae1b397455218e
MangTomas23 <matos.adrianpaul@gmail.com>
2016-01-02 16:00:35 +0800

    Migration: Add teacher_id

--- database/migrations/2016_01_02_062227_create_grades_table.php
+++ database/migrations/2016_01_02_062227_create_grades_table.php
@@ -18,6 +18,9 @@ class CreateGradesTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')
             ->onDelete('cascade');
+            $table->integer('teacher_id')->unsigned();
+            $table->foreign('teacher_id')->references('id')->on('users')
+            ->onDelete('cascade');
             $table->integer('subject_id')->unsigned();
             $table->foreign('subject_id')->references('id')->on('subjects')
             ->onDelete('cascade');
