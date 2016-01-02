ed706514b47eb8b08eeb921d3d835f83428552ad
MangTomas23 <matos.adrianpaul@gmail.com>
2016-01-02 20:13:56 +0800

    Add table

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -4,4 +4,15 @@
 
 <h2>My Grades</h2>
 
+<table class="table table-default">
+	<thead>
+		<th>#</th>
+		<th>Subject</th>
+		<th>1st Quarter</th>
+		<th>2nd Quarter</th>
+		<th>3rd Quarter</th>
+		<th>4th Quarter</th>
+		<th>Average</th>
+	</thead>
+</table>
 @endsection
